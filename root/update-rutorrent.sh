5ba17358f8bbce598530f89ef6854c5c809d55c5
thermitegod <thermitethegod@gmail.com>
2020-09-12 14:57:03 -0700

    backup and diff config.php

--- root/update-rutorrent.sh
+++ root/update-rutorrent.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-#1.0.0
-#2019-12-29
+#1.1.0
+#2020-07-15
 
 emerge -1 rutorrent
+
+cp /var/www/localhost/htdocs/rutorrent/conf/config.php /tmp
 
 rm -rf /var/www/localhost/htdocs/rutorrent
 
 webapp-config --server lighttpd -d rutorrent -I rutorrent 9999
 
 chown -R brandon:brandon /var/www/localhost/htdocs/rutorrent
+
+echo "diff -Naur /tmp/config.php /var/www/localhost/htdocs/rutorrent/conf/config.php"
